059603ab064048fc169d8a0de9b8b2b02444a77d
Menoita99 <rmsma1@iscte-iul.pt>
2019-11-27 19:55:38 +0000

    Revert "Revert "Become a member almost done""
    This reverts commit 80d4d243ff90791051bec14fce100a081a242b79.

--- src/com/beans/BecameMemberBean.java
+++ src/com/beans/BecameMemberBean.java
@@ -5,30 +5,37 @@ import javax.faces.bean.RequestScoped;
 
 import com.utils.EmailSender;
 
+/**
+ * This bean was made for Become a Member page
+ * 
+ * @author Francisco
+ *
+ */
+
 @ManagedBean
 @RequestScoped
 public class BecameMemberBean {
-	
+
 	private String name;
 	private String email;
 	private String aboutYourself;
 	private String errorMessage;
-	
+
 	public String submit() {
-		
-		String content ="Eu " + name + ", quero-me juntar á ACM" + "em seguida, está algo acerca de mim:\n" + aboutYourself;
-		
-		EmailSender.getInstance().sendTextEmail("car31dias321@gmail.com", "Want to join acm", content);
-		
-		
-		return "";
+
+		if (!name.isBlank() && !email.isBlank() && !aboutYourself.isBlank()) {
+
+			String content = "Eu " + name + ", quero-me juntar á ACM" + "em seguida, está algo acerca de mim:\n"
+					+ aboutYourself;
+
+			EmailSender.getInstance().sendTextEmail("car31dias321@gmail.com", "Want to join acm", content);
+			return "home";
+		}else {
+			return "";
+		}
+
 	}
 
-	
-	
-	
-	
-	
 	/**
 	 * @return the name
 	 */
@@ -36,23 +43,12 @@ public class BecameMemberBean {
 		return name;
 	}
 
-	
-	
-	
-	
-	
 	/**
 	 * @param name the name to set
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	
-	
-	
-	
-	
 
 	/**
 	 * @return the email
@@ -61,23 +57,13 @@ public class BecameMemberBean {
 		return email;
 	}
 
-	
-	
-	
-	
-	
 	/**
 	 * @return the aboutYouself
 	 */
 	public String getAboutYourself() {
 		return aboutYourself;
 	}
-	
-	
-	
-	
-	
-	
+
 	/**
 	 * @param aboutYouself the aboutYouself to set
 	 */
@@ -85,23 +71,12 @@ public class BecameMemberBean {
 		this.aboutYourself = aboutYouself;
 	}
 
-	
-	
-	
-	
-	
 	/**
 	 * @param email the email to set
 	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	
-	
-	
-	
-	
 
 	/**
 	 * @return the errorMessage
@@ -110,19 +85,11 @@ public class BecameMemberBean {
 		return errorMessage;
 	}
 
-	
-	
-	
-	
-	
-
 	/**
 	 * @param errorMessage the errorMessage to set
 	 */
 	public void setErrorMessage(String errorMessage) {
 		this.errorMessage = errorMessage;
 	}
-
-
 
 }
